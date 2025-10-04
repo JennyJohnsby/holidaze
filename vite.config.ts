463d8d78a107bf2aa064b7f3c053895bf4c00741
Jenny Johnsby <152171455+JennyJohnsby@users.noreply.github.com>
2025-10-04 16:44:15 +0200

    Configure vite as MPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,22 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  appType: "mpa",
+  base: "",
+  build: {
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./index.html"),
+        login: resolve(__dirname, "./auth/login/index.html"),
+        register: resolve(__dirname, "./auth/register/index.html"),
+        profile: resolve(__dirname, "./profile/index.html"),
+        bookings: resolve(__dirname, "./bookings/index.html"),
+        editBookings: resolve(__dirname, "./bookings/edit/index.html"),
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
