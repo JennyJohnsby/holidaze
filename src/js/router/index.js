0cc457fc9e42ea6e17226797de16ef1e67b610ef
Jenny Johnsby <152171455+JennyJohnsby@users.noreply.github.com>
2025-10-04 16:28:39 +0200

    Revert "router changes"
    This reverts commit aeeec816d646417a1c36fcbfd74d7943431f384f.

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,10 +1,6 @@
 export default async function router(
   pathname = window.location.pathname.split("?")[0]
 ) {
-  if (!pathname.endsWith("/")) {
-    pathname += "/";
-  }
-
   console.log("[Router] navigating to:", pathname);
 
   try {
@@ -14,36 +10,47 @@ export default async function router(
       case "/":
         module = await import("./views/home.js");
         break;
+
       case "/auth/":
         module = await import("./views/auth.js");
         break;
+
       case "/auth/login/":
         module = await import("./views/login.js");
         break;
+
       case "/auth/register/":
         module = await import("./views/register.js");
         break;
+
       case "/venues/":
         module = await import("./views/venues.js");
         break;
+
       case "/venues/create/":
         module = await import("./views/venueCreate.js");
         break;
+
       case "/venues/edit/":
         module = await import("./views/venueEdit.js");
         break;
+
       case "/profile/":
         module = await import("./views/profile.js");
         break;
+
       case "/bookings/":
         module = await import("./views/bookings.js");
         break;
+
       case "/bookings/edit/":
         module = await import("./views/bookingEdit.js");
         break;
+
       case "/bookings/create/":
         module = await import("./views/bookingCreate.js");
         break;
+
       default:
         module = await import("./views/notFound.js");
         break;
@@ -54,6 +61,7 @@ export default async function router(
     }
   } catch (err) {
     console.error("[Router] Failed to load view:", err);
+
     try {
       const module = await import("./views/notFound.js");
       if (module && typeof module.default === "function") {
